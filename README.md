b20b8044a54fb60b55e64ee75076689f58e9f933
Benjamin Flesch <bfitservice@gmail.com>
2020-04-23 16:23:33 +0200

    initial commit, calculate uniq domains from reddit politics

---
+++ README.md
@@ -0,0 +1,3 @@
+# List of Main Stream Media Domains
+
+This project aims to generate a list of domains of main stream media outlets for use in research studies.
---
+++ data/reddit_politics/merge-reddit-politics-domain-lists.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for file in $(ls *.csv); do
+	echo $file;
+	wc -l $file;
+
+	header=$(head -n 1 $file);
+	echo $header;
+
+	IFS="	" read -r -a array <<< "$header";
+	for index in "${!array[@]}"; do
+		echo "item: $index ${array[index]}";
+		if [ "${array[index]}" = "Website" ] || [ "${array[index]}" = "URL" ]; then
+			echo "URL found in column $index";
+			break;
+		fi;
+	done;
+
+	tail -n +2 $file | cut -f $((index+1)) | cut -d '/' -f 1 | sort | uniq > "$file.domains.txt";
+done
+
+# merge all domain files into one
+cat *.domains.txt | sort | uniq > ../../output/reddit-politics.txt
